8a783ba0e0b69424ae93a1533532a39d777b5ffa
LoSti's World <david.in.bergen@gmail.com>
2023-02-25 03:59:31 +0100

    update magneto class

--- src/Magneto.ts
+++ src/Magneto.ts
@@ -1,13 +1,35 @@
 export class Magneto {
   domEl: Element;
+  distance: number;
+  viewportHeight: number;
 
-  constructor(element: Element) {
+  constructor(element: Element, distance: number) {
     this.domEl = element;
-    this.trigger()
+    this.distance = distance;
+    this.viewportHeight = window.innerHeight;
+    this.observer();
+    this.resize();
   }
 
-  trigger() {
-    console.log('loaded');
-    
+  observer() {
+    const options = {
+      rootMargin: `${this.viewportHeight - (this.distance / 2)}px 0px`,
+      threshold: 0.5
+    }
+    const observer = new IntersectionObserver((entries: IntersectionObserverEntry[]) => {
+      entries.forEach((entry) => {
+        
+        if (!entry.isIntersecting) {
+          console.log('not in viewport', options.rootMargin);
+        }
+        if (entry.isIntersecting) {
+          console.log('in viewport', options.rootMargin);
+        }
+      })
+    }, options);
+
+    observer.observe(this.domEl);
   }
+
+  resize() {}
 }
