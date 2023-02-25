a9cfb080c6d0eef7f3011987edd41a70d6b752b3
LoSti's World <david.in.bergen@gmail.com>
2023-02-25 17:01:47 +0100

    update

--- src/Magneto.ts
+++ src/Magneto.ts
@@ -1,14 +1,16 @@
 export class Magneto {
-  domEl: Element;
-  distance: number;
-  viewportHeight: number;
-  domElRect: DOMRect;
+  private readonly triggerArea: number;
+  private readonly domEl: HTMLElement;
+  private domElRect: DOMRect;
+  posX: number;
+  posY: number;
 
-  constructor(element: Element, distance: number) {
-    this.domEl = element;
-    this.distance = distance;
-    this.viewportHeight = window.innerHeight;
+  constructor(domEl: HTMLElement, triggerArea: number = 200) {
+    this.domEl = domEl;
     this.domElRect = this.domEl.getBoundingClientRect();
+    this.triggerArea = triggerArea;
+    this.posX = 0;
+    this.posY = 0;
     this.observer();
     this.resize();
   }
@@ -16,30 +18,62 @@ export class Magneto {
   observer() {
     const options = {
       rootMargin: '0px',
-      threshold: 0.1
-    }
+      threshold: 0,
+    };
 
-    const observer = new IntersectionObserver((entries: IntersectionObserverEntry[]) => {
-      entries.forEach((entry) => {
-
-        if (!entry.isIntersecting) {
-          console.log('not in viewport');
-        }
-        if (entry.isIntersecting) {
-          window.addEventListener('mousemove', (e) => {
-            console.log(e.x, e.y);
-          });
-          console.log('in viewport');
-        }
-      })
+    const observer = new IntersectionObserver(entries => {
+      entries.forEach(entry => {
+        entry.isIntersecting ? this.startMouseTracker() : this.stopMouseTracker();
+        this.domEl.classList.toggle('active', entry.isIntersecting)
+      });
     }, options);
-
     observer.observe(this.domEl);
   }
 
-  mouseTracker(x: number, y: number) {
-
+  startMouseTracker() {
+    window.addEventListener('mousemove', this.onMouseMove);
   }
 
-  resize() { }
+  stopMouseTracker() {
+    window.removeEventListener('mousemove', this.onMouseMove);
+  }
+
+  onMouseMove(e: MouseEvent) {
+    const mousePos = { x: e.pageX, y: e.pageY };
+    console.log(mousePos);
+    
+    const triggerAreaFromMouseToCenter = this.mouseTracker(mousePos);
+
+    (triggerAreaFromMouseToCenter < this.triggerArea) 
+      ? this.setTargetHolder(mousePos)
+      : this.setTargetHolder({ x: 0, y: 0 });
+  };
+
+  setTargetHolder(mousePosition: { x: number; y: number }) {
+    const targetHolder = {
+      x: (mousePosition.x - (this.domElRect.left + this.domElRect.width / 2)) * 0.2,
+      y: (mousePosition.y - (this.domElRect.top + this.domElRect.height / 2)) * 0.2,
+    };
+
+    this.posX = targetHolder.x;
+    this.posY = targetHolder.y;
+    
+    this.domEl.style.setProperty('--posX', `${this.posX}px`);
+    this.domEl.style.setProperty('--posY', `${this.posY}px`);
+  }
+
+  mouseTracker(mousePosition: { x: number; y: number; }) {
+    return Math.hypot(
+      mousePosition.x -
+        (this.domElRect.left + this.domElRect.width / 2),
+      mousePosition.y -
+        (this.domElRect.top + this.domElRect.height / 2)
+    );
+  }
+
+  resize() {
+    window.addEventListener('resize', () => {
+      this.domElRect = this.domEl.getBoundingClientRect();
+    });
+  }
 }
